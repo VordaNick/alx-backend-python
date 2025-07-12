4436da961702b61524bfe0c96862cc7fc61f2234
VordaNick <vordanick@gmail.com>
2025-07-12 10:15:54 +0100

    Create  0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+ def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Check if a query was passed as a positional or keyword argument
+        query = kwargs.get('query')
+        if not query and args:
+            query = args[0]
+
+        print(f"[LOG] Executing SQL Query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
